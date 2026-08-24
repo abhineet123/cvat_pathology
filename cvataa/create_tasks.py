a19920527f2705c99c4d45b85842565209760b19
abhineet123 <asingh1@ualberta.ca>
2026-08-23 19:52:49 -0600

    update cvat tasks creation to use low-level api and decrease cvat server failures

--- cvataa/create_tasks.py
+++ cvataa/create_tasks.py
@@ -1,20 +1,35 @@
 import os
-import paramparse
-
+import time
 from tqdm import tqdm
 from datetime import datetime
 
-from cvat_sdk import make_client, models
-from cvat_sdk.core.proxies.tasks import ResourceType, Task
+import paramparse
 
-from cell_seg_utils import CVATAuth, Filter, to_str
+from cvat_sdk import make_client
+from cvat_sdk.core.proxies.tasks import ResourceType
+from cvat_sdk.api_client import exceptions
 
-from pprint import pprint
-
-from cvat_sdk.api_client import Configuration, ApiClient, exceptions, models
+from cell_seg_utils import (
+    CVATAuth,
+    Filter,
+    to_str,
+    sleep_with_pbar,
+    linux_path,
+    create_project_lla,
+    get_cvat_tasks_hla,
+    delete_cvat_task_hla,
+    get_cvat_projects_hla,
+    chunks,
+)
 
 
 class Params(paramparse.CFG):
+    class Chunk:
+        def __init__(self):
+            self.count = 0
+            self.size = 0
+            self.tolerance = 0
+
     def __init__(self):
         paramparse.CFG.__init__(self)
 
@@ -26,10 +41,15 @@ class Params(paramparse.CFG):
         self.recursive = 0
         self.multi = 0
 
+        self.start_dir_id = 0
+        self.end_dir_id = -1
+        self.excp_wait_t = 120
+
         self.project_name = ""
 
+        self.chunk = Params.Chunk()
+
         self.model_suffixes = []
-        self.max_images = 0
 
         # self.model_suffix = "stardist"
         # self.model_suffix = "instanseg"
@@ -45,82 +65,37 @@ class Params(paramparse.CFG):
         ]
 
 
-def linux_path(*args, **kwargs):
-    return os.path.join(*args, **kwargs).replace(os.sep, "/")
+def create_cvat_project(client, client_cfg, project_name, label_names, label_cols):
+    print("get_projects...")
+    projects_dict, project_name_to_id, project_name_to_dict = get_cvat_projects_hla(client)
 
+    if project_name not in project_name_to_id:
+        # raise AssertionError(f"Nonexistent project: {project_name}")
+        timestamp = datetime.now().strftime("%y%m%d_%H%M%S")
+        print(f"{timestamp} creating project: {project_name}")
 
-def create_project_lla(cfg_dict, name, label_names, label_cols):
-    if label_names is None:
-        label_names = [
-            "nucleus",
-        ]
-
-    assert len(label_cols) >= len(label_names), "Insufficient number of label_cols"
-
-    configuration = Configuration(**cfg_dict)
-    with ApiClient(configuration) as api_client:
-        project_write_request = models.ProjectWriteRequest(
-            name=name,
-            labels=[
-                models.PatchedLabelRequest(
-                    id=label_id + 1,
-                    name=label_name,
-                    color=label_cols[label_id],
-                    type="any",
-                )
-                for label_id, label_name in enumerate((label_names))
-            ],
+        project_dict = create_project_lla(
+            client_cfg,
+            project_name,
+            label_names=label_names,
+            label_cols=label_cols,
         )
-        try:
-            (data, response) = api_client.projects_api.create(
-                project_write_request,
-                # x_organization=x_organization,
-                # org=org,
-                # org_id=org_id,
-            )
-            return data
-        except exceptions.ApiException as e:
-            print("Exception when calling ProjectsApi.create(): %s\n" % e)
 
+        project_id = project_dict["id"]
 
-def create_project_hla(api_client, name):
-    project_spec = dict(
-        name=name,
-        labels=[
-            dict(
-                name="nucleus",
-                color="#fafa37",
-                type="any",
-            )
-        ],
-    )
-    try:
-        project = api_client.projects.create(
-            project_spec,
-        )
-        pprint(project)
-    except exceptions.ApiException as e:
-        print("Exception when calling projects.create(): %s\n" % e)
+        projects_dict, project_name_to_id, project_name_to_dict = get_cvat_projects_hla(client)
 
+        assert project_id == project_name_to_id[project_name]
 
-def get_projects(client):
-    projects_dict = [project.__dict__ for project in client.projects.list()]
-    project_name_to_id = {
-        project["_model"]["name"]: project["_model"]["id"] for project in projects_dict
-    }
-    project_name_to_dict = {
-        project["_model"]["name"]: project["_model"] for project in projects_dict
-    }
-    return projects_dict, project_name_to_id, project_name_to_dict
+    # tasks_meta = [task.get_meta().to_dict() for task in tasks]
+    # projects_preview = [project.get_preview() for project in projects]
 
+    project_id = project_name_to_id[project_name]
 
-def chunks(lst, n):
-    """
-    Yield successive n-sized chunks from lst.
-    https://stackoverflow.com/a/312464
-    """
-    for i in range(0, len(lst), n):
-        yield lst[i : i + n]
+    # project_dict = project_name_to_dict[project_name].to_dict()
+    # project_tasks = project_dict["tasks"]
+
+    return project_id
 
 
 def main():
@@ -170,46 +145,38 @@ def main():
             if project_suffix:
                 project_name = f"{project_name}-{project_suffix}"
 
-            projects_dict, project_name_to_id, project_name_to_dict = get_projects(client)
-
-            tasks_dict = [task.__dict__ for task in client.tasks.list()]
-            task_name_to_id = {task["_model"]["name"]: task["_model"]["id"] for task in tasks_dict}
-
-            if project_name not in project_name_to_id:
-                # raise AssertionError(f"Nonexistent project: {project_name}")
-                timestamp = datetime.now().strftime("%y%m%d_%H%M%S")
-                print(f"{timestamp} creating project: {project_name}")
-
-                project_dict = create_project_lla(
-                    client_cfg,
-                    project_name,
-                    label_names=params.model_suffixes if params.multi else None,
-                    label_cols=params.label_cols,
-                )
-
-                project_id = project_dict["id"]
-
-                projects_dict, project_name_to_id, project_name_to_dict = get_projects(client)
-
-                assert project_id == project_name_to_id[project_name]
-
-            # tasks_meta = [task.get_meta().to_dict() for task in tasks]
-            # projects_preview = [project.get_preview() for project in projects]
-
-            project_id = project_name_to_id[project_name]
-            project_dict = project_name_to_dict[project_name]
+            project_id = create_cvat_project(
+                client,
+                client_cfg,
+                project_name,
+                label_names=params.model_suffixes if params.multi else None,
+                label_cols=params.label_cols,
+            )
+            task_name_to_id = get_cvat_tasks_hla(client)
 
             n_subdirs = len(subdirs)
 
             assert n_subdirs, "no subdirs found"
             image_exts = ["jpg", "jpeg", "bmp", "png", "tif", "webp"]
 
-            for subdir_id, subdir in enumerate(subdirs):
+            subdir_id = 0
+
+            while subdir_id < n_subdirs:
+                subdir = subdirs[subdir_id]
                 task_name = os.path.relpath(subdir, dir_path)
                 task_name = f"{project_name}-{task_name}"
 
+                if subdir_id < params.start_dir_id:
+                    print(f"{subdir_id+1} / {n_subdirs} skipping task: {task_name}")
+                    subdir_id += 1
+                    continue
+
+                if subdir_id > params.end_dir_id >= 0:
+                    break
+
                 if task_name in task_name_to_id:
                     print(f"{subdir_id+1} / {n_subdirs} skipping existing task: {task_name}")
+                    subdir_id += 1
                     continue
 
                 image_paths = [
@@ -220,6 +187,7 @@ def main():
 
                 if not image_paths:
                     print(f"Skipping subdir with no images: {subdir}")
+                    subdir_id += 1
                     continue
 
                 n_images = len(image_paths)
@@ -229,40 +197,86 @@ def main():
                     f"{timestamp} creating task for subdir {subdir_id+1} / {n_subdirs} with {n_images} images: {task_name}"
                 )
 
-                if params.max_images > 0 and n_images > params.max_images:
-                    image_paths_chunks = list(chunks(image_paths, params.max_images))
+                if params.chunk.size > 0 and n_images > params.chunk.size:
+                    image_paths_chunks = list(chunks(image_paths, params.chunk.size))
+
+                    """if the last chunk is too small, subsume it within the second last chunk"""
+
+                    if len(image_paths_chunks[-1]) < int(
+                        params.chunk.tolerance * params.chunk.size
+                    ):
+                        image_paths_chunks[-2] += image_paths_chunks[-1]
+                        del image_paths_chunks[-1]
+
                     n_chunks = len(image_paths_chunks)
+
+                    if n_chunks >= params.chunk.count > 0:
+                        n_chunks = params.chunk.count
+
                     print(
-                        f"splitting {n_images} images into {n_chunks} chunks of size <= {params.max_images}"
+                        f"splitting {n_images} images into {n_chunks} chunks of size <= {params.chunk.size} (tolerance: {params.chunk.tolerance})"
                     )
 
-                    for chunk_id, image_paths_chunk in enumerate(image_paths_chunks):
+                    chunk_id = 0
+                    while chunk_id < n_chunks:
+                        image_paths_chunk = image_paths_chunks[chunk_id]
+                        chunk_task_name = f"{task_name}-chunk_{chunk_id+1:02d}"
+                        if chunk_task_name in task_name_to_id:
+                            print(
+                                f"\tchunk {chunk_id+1} / {n_chunks} skipping existing task: {chunk_task_name}"
+                            )
+                            chunk_id += 1
+                            continue
+
                         n_chunk_images = len(image_paths_chunk)
                         task_spec = {
-                            "name": f"{task_name}-chunk_{chunk_id+1:02d}",
+                            "name": chunk_task_name,
                             "project_id": project_id,
                         }
                         print(
                             f"\tchunk {chunk_id+1} / {n_chunks} with {n_chunk_images} images: {task_spec['name']}"
                         )
-                        task = client.tasks.create_from_data(
-                            spec=task_spec,
-                            resource_type=ResourceType.LOCAL,
-                            resources=image_paths_chunk,
-                        )
-                        assert task.size == n_chunk_images
+                        try:
+                            task = client.tasks.create_from_data(
+                                spec=task_spec,
+                                resource_type=ResourceType.LOCAL,
+                                resources=image_paths_chunk,
+                                # pbar=TQDMProgressReporter(),
+                            )
+                        except exceptions.ServiceException as e:
+                            print(
+                                f"\n\nTask creation failed:\n{e}\nWaiting {params.excp_wait_t} seconds before trying again\n\n"
+                            )
+                            sleep_with_pbar(params.excp_wait_t)
+                            task_name_to_id = delete_cvat_task_hla(client, chunk_task_name)
+                            continue
+                        else:
+                            assert task.size == n_chunk_images
+                            chunk_id += 1
 
                 else:
                     task_spec = {
                         "name": task_name,
                         "project_id": project_id,
                     }
-                    task = client.tasks.create_from_data(
-                        spec=task_spec,
-                        resource_type=ResourceType.LOCAL,
-                        resources=image_paths,
-                    )
-                    assert task.size == n_images
+                    try:
+                        task = client.tasks.create_from_data(
+                            spec=task_spec,
+                            resource_type=ResourceType.LOCAL,
+                            resources=image_paths,
+                            # pbar=TQDMProgressReporter(),
+                        )
+                    except exceptions.ServiceException as e:
+                        print(
+                            f"\n\nTask creation failed:\n{e}\nWaiting {params.excp_wait_t} seconds before trying again\n\n"
+                        )
+                        sleep_with_pbar(params.excp_wait_t)
+                        task_name_to_id = delete_cvat_task_hla(client, task_name)
+                        continue
+                    else:
+                        assert task.size == n_images
+
+                subdir_id += 1
 
                 # return
 
