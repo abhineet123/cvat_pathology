b5e209536626c80e9919e01d9e99150bc2f390b1
abhineet123 <asingh1@ualberta.ca>
2026-08-23 19:48:53 -0600

    minor fixes in the stardist cli script

--- cvataa/stardist_cli.py
+++ cvataa/stardist_cli.py
@@ -14,21 +14,43 @@ import cvat_sdk.models as models
 import cvat_sdk.auto_annotation as cvataa
 from csbdeep.utils import normalize
 
-from cell_seg_utils import instance_mask_to_cells, CellSegCLIBase
+from cell_seg_cli import CellSegCLIBase
 
 
 class StardistCLI(CellSegCLIBase):
-    def __init__(self, task_name, label_name, n_frames, verbose=True, **kwargs) -> None:
-        CellSegCLIBase.__init__(self, task_name, label_name, n_frames, verbose, "StarDist")
+    def __init__(self, **kwargs) -> None:
+        CellSegCLIBase.__init__(self, name="stardist", **kwargs)
         self.predictor = StarDist2D.from_pretrained("2D_versatile_he")
 
     def detect(
         self, context: cvataa.DetectionFunctionContext, image: PIL.Image.Image, return_raw=False
     ) -> list[models.LabeledShapeRequest]:
-        image_np = normalize(np.array(image))
-        labeled_output, _ = self.predictor.predict_instances(image_np)
 
-        results = instance_mask_to_cells(labeled_output, return_raw)
+        if context.frame_name == "B16 [x=105984,y=30464,w=256,h=256].png":
+            """
+            this empty image causes weird internal failure:
+            terminate called recursively
+            terminate called recursively
+            terminate called recursively
+            terminate called recursively
+            terminate called recursively
+            terminate called recursively
+            Aborted (core dumped)
+            """
+
+            results = []
+            self.update_status(context, results)
+            return results
+
+        image_np = normalize(np.array(image))
+        try:
+            labeled_output, _ = self.predictor.predict_instances(image_np)
+        except:
+            results = []
+        else:
+            results = self.instance_mask_to_cells(
+                labeled_output,
+            )
 
         self.update_status(context, results)
         return results
